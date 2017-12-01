f93091f22f87f75b1d97efd41428b12e8f890e0b
lgants <lgants@gmail.com>
2017-11-30 19:22:59 -0600

    updated user picker

--- src/components/UserPicker.js
+++ src/components/UserPicker.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { observer } from 'mobx-react';
 
 const UserPicker = observer(props =>
-  <div>
   <select
     value={props.user ? props.user.id : ""}
     onChange={e => props.onChange(e.target.value)}
   >
     <option value="">-none-</option>
-    {props.store.users.values().map(user => <option value={user.id}>{user.name}</option>)}
+    {props.store.users.values().map(user =>
+      <option value={user.id}>{user.name}</option>
+    )}
   </select>
-  </div>
 )
 
 export default UserPicker;
